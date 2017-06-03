dc6c39643bc80029304542a8174f386e5fe47810
luifer <luifer.2341@gmail.com>
2017-06-03 10:31:58 -0400

    devise user added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
-  resources :establishments
-  resources :players
-  resources :savings
-  resources :fields
-  resources :attendances
-  resources :teams
-  resources :savingfields
+  
+
+  devise_for :users
+  authenticated :user do
+  	root 'welcome#index'
+  	resources :establishments
+    resources :players
+    resources :savings
+    resources :fields
+    resources :attendances
+    resources :teams
+    resources :savingfields
+  end
+
+
+   unauthenticated :user do
+   	devise_scope :user do
+  		root "welcome#unregistered", as: :unregistered_root
+   	end
+
+  end
+
   
 end
